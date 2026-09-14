303b81eb10fe6185bbb90dfb92abc5ef316eba2c
diego221992 <diegoalmeidaunlock@gmail.com>
2026-09-14 16:02:42 -0400

    Change API_URL to use HTTPS

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   //API_URL: "http://192.168.0.100:3002",
-  API_URL: "http://api.juan-app.online", // O el puerto donde corra tu backend (Node.js, PHP, etc.)
+  API_URL: "https://api.juan-app.online", // O el puerto donde corra tu backend (Node.js, PHP, etc.)
   ENVIRONMENT: "development"
 };
